909e9d2260f05e7980ccfa590904e74e54ebd492
Huynh Truong <huynhphuoctruongk17086@gmail.com>
2023-06-09 13:45:20 +0700

    update file

--- backend/dist/src/consts/common.const.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AVATAR = void 0;
-exports.AVATAR = 'https://res.cloudinary.com/huynhtruong01/image/upload/v1683210195/trend-avatar-1_ywcrpd.jpg';
