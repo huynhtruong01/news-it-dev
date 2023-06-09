49b2fc83b42a1947ed7fff8b9eacee56a1d9442c
Huynh Truong <huynhphuoctruongk17086@gmail.com>
2023-06-09 13:39:24 +0700

    update

--- backend/src/config/data.ts
+++ backend/src/config/data.ts
@@ -3,6 +3,7 @@ import { DataSource } from 'typeorm'
 
 export const AppDataSource = new DataSource({
     type: 'mysql',
+    host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
